e7e9db22843fddc7e2a68c64e25e3975b6f6b93e
Rodrigo Díaz <rdiazv89@gmail.com>
2018-03-20 22:50:48 -0300

    Creates repo and installs dependencies

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ const path = require('path')
 const camelCase = require('uppercamelcase')
 const readDir = require('recursive-readdir')
 const validateNpmPackageName = require('validate-npm-package-name')
+const chalk = require('chalk')
+const ora = require('ora')
 const pkg = require('./package.json')
+const exec = require('child_process').exec
 
 dot.templateSettings.strip = false
 
+const spinner = ora()
+const organization = '5rabbits' // Maybe this could be configurable
+const originalCwd = process.cwd()
 let libraryName
 let packageName
 let componentName
+let templatePath
+let projectCwd
 
 program
   .version(pkg.version)
@@ -32,40 +40,81 @@ function validateName(name) {
   const validation = validateNpmPackageName(name)
 
   if (validation.errors) {
-    console.error(validation.errors.join("\n"))
+    console.error(chalk.red('Project name can only contain URL-friendly characters'))
     process.exit(1)
   }
 }
 
 function createProject(name) {
-  validateName(name)
+  console.log(`${chalk.yellow('@5rabbits/create-lib')} v${pkg.version}\n`)
 
-  // Maybe this could be configurable
-  const organization = '5rabbits'
+  validateName(name)
 
   libraryName = name
   packageName = `@${organization}/${libraryName}`
   componentName = camelCase(libraryName)
+  templatePath = path.resolve(__dirname, 'template')
+  projectCwd = path.join(process.cwd(), libraryName)
 
-  const templateValues = {
-    libraryName,
-    packageName,
-    componentName,
-    organization,
-  }
+  createProjectFiles()
+    .then(createGitRepo)
+    .then(installDependencies)
+}
 
-  const templatePath = path.resolve(__dirname, 'template')
-  const projectCwd = path.join(process.cwd(), libraryName)
+function installDependencies() {
+  spinner.start('Installing dependencies')
 
-  readDir(templatePath, (err, files) => {
-    files.forEach(file => {
-      const filePath = file.replace(templatePath, '')
-      const fileContents = fs.readFileSync(file)
+  return projectExec('yarn')
+    .then(() => spinner.succeed('Dependencies installed'))
+}
 
-      fs.outputFileSync(
-        path.join(projectCwd, evaluateFilePath(filePath)),
-        dot.template(fileContents)(templateValues)
-      )
+function createGitRepo() {
+  spinner.start('Creating git repo')
+
+  return projectExec('git init')
+    .then(() => projectExec('git add .'))
+    .then(() => projectExec('git commit -m "Initial commit"'))
+    .then(() => spinner.succeed('Git repo initialized'))
+}
+
+function createProjectFiles(name) {
+  return new Promise(resolve => {
+    spinner.start(`Creating project ${chalk.bold(libraryName)}`)
+
+    const templateValues = {
+      libraryName,
+      packageName,
+      componentName,
+      organization,
+    }
+
+    readDir(templatePath)
+      .then(files => {
+        files.forEach(file => {
+          const filePath = file.replace(templatePath, '')
+          const fileContents = fs.readFileSync(file)
+
+          fs.outputFileSync(
+            path.join(projectCwd, evaluateFilePath(filePath)),
+            dot.template(fileContents)(templateValues)
+          )
+        })
+
+        spinner.succeed(`Project ${chalk.bold(libraryName)} created`)
+        resolve()
+      })
+  })
+}
+
+function projectExec(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, { cwd: projectCwd }, function(error, stdout) {
+      if (error) {
+        reject(error)
+      }
+      else {
+        resolve(stdout)
+      }
     })
   })
 }
