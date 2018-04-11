4e447e946a6ca50af102ae0cf479fd1be0f6a451
Rodrigo Díaz <rdiazv89@gmail.com>
2018-04-11 18:35:23 -0300

    More tests examples

--- template/src/__tests__/App.test.js
+++ template/src/__tests__/App.test.js
@@ -8,4 +8,20 @@ describe(App, () => {
 
     expect(component).toHaveText('Click here')
   })
+
+  describe('props.className', () => {
+    it('should assign the specified className', () => {
+      const component = shallow(<App className="some-classname" />)
+
+      expect(component.find('button')).toHaveClassName('some-classname')
+    })
+  })
+
+  describe('props.theme', () => {
+    it('should assign a className for the specified theme', () => {
+      const component = shallow(<App theme="primary" />)
+
+      expect(component.find('button')).toHaveClassName('theme--primary')
+    })
+  })
 })
